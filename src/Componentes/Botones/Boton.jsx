964c893061420c6d729dd57984e2f67a8b58dea7
Facundo Almiñana <facu0797@gmail.com>
2024-08-06 02:36:39 -0300

    Agregando cv

--- src/Componentes/Botones/Boton.jsx
+++ src/Componentes/Botones/Boton.jsx
@@ -1,11 +1,14 @@
 import { IoMdSend } from "react-icons/io";
+import cvFacundo from "/CV-FACUNDO ALMINANA.pdf"
 
 const Boton = () => {
-    return ( 
-        <button className="boton-cv">
-            <IoMdSend />
-            <div className="text">Descargar CV</div>
-        </button>    
+    return (
+        <a download="cv-facundo-alminana" href={cvFacundo}>
+            <button className="boton-cv">
+                <IoMdSend />
+                <div className="text">Descargar CV</div>
+            </button>    
+        </a>
     );
 }
  
